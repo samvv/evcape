73cbabadc1a590b5201086b305da4597d1473f4e
Sam Vervaeck <samvv@pm.me>
2024-09-02 00:37:01 +0200

    Fix missing 'lib' variable in nixos.nix

--- nixos.nix
+++ nixos.nix
@@ -1,30 +1,31 @@
-{
-  options.services.evcape = with lib; {
-    enable = mkEnableOption "enable the evcape key remapper service";
-    log_level = mkOption {
-      type = types.int;
-      default = 5;
-      description = "how much messages to log in the console";
-    };
-  };
+{ config, lib, pkgs, ... }: with lib;
 
-  cfg = config.services.float;
+  let cfg = config.services.float; in {
 
-  config = lib.mkIf cfg.enable {
-    systemd.services.evcape = {
-      description = "evcape key remapper";
-      wantedBy = [ "multi-user.tarrget" ];
-      after = [ "udev.service" ];
-      environment = {
-        EVCAPE_LOG_LEVEL = "${cfg.log_level}";
-      };
-      serviceConfig = {
-        Type = "simple";
-        ExecStart = "${cfg.package}/bin/evcape";
-        Restart = "on-failure";
-        ProtectHome = "read-only";
+    options.services.evcape = {
+      enable = mkEnableOption "enable the evcape key remapper service";
+      log_level = mkOption {
+        type = types.int;
+        default = 5;
+        description = "how much messages to log in the console";
       };
     };
-  };
 
-}
+    config = mkIf cfg.enable {
+      systemd.services.evcape = {
+        description = "evcape key remapper";
+        wantedBy = [ "multi-user.tarrget" ];
+        after = [ "udev.service" ];
+        environment = {
+          EVCAPE_LOG_LEVEL = "${cfg.log_level}";
+        };
+        serviceConfig = {
+          Type = "simple";
+          ExecStart = "${cfg.package}/bin/evcape";
+          Restart = "on-failure";
+          ProtectHome = "read-only";
+        };
+      };
+    };
+
+  }
